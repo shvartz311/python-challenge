6898e0630041cab875606452138398362f9a3b0f
Ory Shvartz <52048043+shvartz311@users.noreply.github.com>
2022-03-13 16:47:42 +0200

    Add files via upload

---
+++ pythonchallenge.py
@@ -0,0 +1,70 @@
+import json
+import requests
+import csv
+from threading import Thread
+from time import perf_counter
+
+user = "myawesomeuser"
+passw = "Admin1234!"
+platform_base_url = "https://myawesomeproject.jfrog.io"
+repo_name = "libs-release-local"
+params = {'user':user, 'passw':passw,'api':'no','list':'','deep':'1'}
+string_to_find = "This is a short story !"
+
+def generateToken(func):
+    def inner():
+        InitialGetToken = requests.post(url=f'{platform_base_url}/artifactory/api/security/token',auth=(user, passw),data={"username":user, "expires_in": 5, "refreshable": True})
+        InitialGetTokenJson = InitialGetToken.json()
+        global access_token; access_token = InitialGetTokenJson['access_token']
+        global refresh_token; refresh_token = InitialGetTokenJson['refresh_token']
+        print("Generating token..")
+        return func()
+    return inner
+
+@generateToken
+def retrieveUri():
+    initialGetRequest = requests.get(f"https://myawesomeproject.jfrog.io/artifactory/api/storage/{repo_name}",params=params,auth=(user, access_token))
+    initialGetResponse = initialGetRequest.json()
+    uri_list = [x['uri'] for x in initialGetResponse['files']]
+    return uri_list
+
+def getStoryIntoCsv(listOfUris, story):
+    try:
+        GetContentOfFile = requests.get(url=f'{platform_base_url}/artifactory/{repo_name}{story}',
+                            headers={"Authorization":f"Bearer {access_token}"},
+                            stream=True)
+        if GetContentOfFile.status_code == 200:
+            ContentOfFile = GetContentOfFile.iter_content(chunk_size=len(string_to_find))
+            for content in ContentOfFile:
+                if string_to_find in content.decode():
+                    final_lst.append(f'\"{platform_base_url}{story}\" | \"{repr(GetContentOfFile.content.decode())}\"\n')
+                    #final_lst = lambda T: final_lst.append(f'\"{platform_base_url}{story}\" | \"{repr(GetContentOfFile.content.decode())}\"\n') if string_to_find in content.decode() else None
+                else:
+                    break
+    except Exception:
+        pass
+
+def main():
+    global f; f = open('stories.csv', 'w')
+    f.write(f'File Name, Story Txt (without the title)\n')
+    listOfUris = retrieveUri()
+    global final_lst; final_lst = []
+    
+    threads = [Thread(target=getStoryIntoCsv, args=(listOfUris, i)) for i in listOfUris]  
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    for i in final_lst[::-1]:
+        if "Nothing" in i:
+            f.write(i)
+
+if __name__ == "__main__":
+    start_time = perf_counter()
+    main()
+    end_time = perf_counter()
+    print("*                  *                 *")
+    print(f'It took {end_time-start_time: 0.4f} second(s) to complete.')
+    print("*                  *                 *")
---
+++ script.sh
@@ -0,0 +1,2 @@
+python pythonchallenge.py
+cat stories.csv
