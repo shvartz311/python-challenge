5e0d9383b05d260bd249258cafa63452026753ec
Ory Shvartz <orys@jfrog.com>
2022-03-20 09:54:25 +0200

    Updated efficiency, filter by size

--- pythonchallenge.py
+++ pythonchallenge.py
@@ -10,45 +10,41 @@ platform_base_url = "xxx"
 repo_name = "libs-release-local"
 params = {'user':user, 'passw':passw,'api':'no','list':'','deep':'1'}
 string_to_find = "This is a short story !"
+final_lst = []
+f = open('stories.csv', 'w')
 
 def generateToken(func):
     def inner():
         InitialGetToken = requests.post(url=f'{platform_base_url}/artifactory/api/security/token',auth=(user, passw),data={"username":user, "expires_in": 5, "refreshable": True})
         InitialGetTokenJson = InitialGetToken.json()
         global access_token; access_token = InitialGetTokenJson['access_token']
-        global refresh_token; refresh_token = InitialGetTokenJson['refresh_token']
         print("Generating token..")
         return func()
     return inner
 
 @generateToken
 def retrieveUri():
-    initialGetRequest = requests.get(f'{platform_base_url}/artifactory/api/storage/{repo_name}',params=params,auth=(user, access_token))
+    initialGetRequest = requests.get(url=f'{platform_base_url}/artifactory/api/storage/{repo_name}',params=params,auth=(user, access_token))
     initialGetResponse = initialGetRequest.json()
-    uri_list = [x['uri'] for x in initialGetResponse['files']]
+    uri_list = [x['uri'] for x in initialGetResponse['files'] if x['size'] < 80000]
     return uri_list
 
 def getStoryIntoCsv(listOfUris, story):
     try:
-        GetContentOfFile = requests.get(url=f'{platform_base_url}/artifactory/{repo_name}{story}',
-                            headers={"Authorization":f"Bearer {access_token}"},
-                            stream=True)
+        GetContentOfFile = requests.get(url=f'{platform_base_url}/artifactory/{repo_name}{story}',headers={"Authorization":f"Bearer {access_token}"},stream=True)
         if GetContentOfFile.status_code == 200:
             ContentOfFile = GetContentOfFile.iter_content(chunk_size=len(string_to_find))
             for content in ContentOfFile:
                 if string_to_find in content.decode():
                     final_lst.append(f'\"{platform_base_url}{story}\" | \"{repr(GetContentOfFile.content.decode())}\"\n')
-                    #final_lst = lambda T: final_lst.append(f'\"{platform_base_url}{story}\" | \"{repr(GetContentOfFile.content.decode())}\"\n') if string_to_find in content.decode() else None
                 else:
                     break
     except Exception:
         pass
 
 def main():
-    global f; f = open('stories.csv', 'w')
     f.write(f'File Name, Story Txt (without the title)\n')
     listOfUris = retrieveUri()
-    global final_lst; final_lst = []
     
     threads = [Thread(target=getStoryIntoCsv, args=(listOfUris, i)) for i in listOfUris]  
     for thread in threads:
